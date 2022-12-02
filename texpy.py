1854818208208b1cba4629bca4ef4ed3f5c5ce91
Lucija Marevic <lucijamarevic@gmail.com>
2022-12-02 23:02:14 +0100

    pandas.DataFrame u latex tablicu

--- texpy.py
+++ texpy.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 # kao parametar prima pandas dataframe, a vraca tablicu
 def make_table(data):
+    tbl = str(data.columns[0]) + " & " + str(data.columns[1]) + " \\\\ \n"
     arr = np.array(data)
     a,b  = arr.shape[0], arr.shape[1]
-    tbl = ""
     for i in range(a):
         for j in range(b-1):
             tbl += str(arr[i][j]) + " & "
