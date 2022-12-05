ca27015b17ccd266ac851ebaec2e48ed8551ee5b
Lucija Marevic <lucijamarevic@gmail.com>
2022-12-05 13:18:05 +0100

    ispravljen bug u prvom redu tablice

--- texpy.py
+++ texpy.py
@@ -3,9 +3,12 @@ import pandas as pd
 
 # kao parametar prima pandas dataframe, a vraca tablicu
 def make_table(data):
-    tbl = str(data.columns[0]) + " & " + str(data.columns[1]) + " \\\\ \n"
+    tbl = ""
     arr = np.array(data)
     a,b  = arr.shape[0], arr.shape[1]
+    for i in range(b-1):
+        tbl += str(data.columns[i]) + " & " 
+    tbl += str(data.columns[b-1]) + " \\\\ \n"
     for i in range(a):
         for j in range(b-1):
             tbl += str(arr[i][j]) + " & "
