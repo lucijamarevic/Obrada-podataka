55a92ac356b0488454feed082de6a54fa943684a
Lucija Marevic <lucijamarevic@gmail.com>
2022-12-05 13:26:19 +0100

    skracen zapis na 5 decimala

--- texpy.py
+++ texpy.py
@@ -7,12 +7,12 @@ def make_table(data):
     arr = np.array(data)
     a,b  = arr.shape[0], arr.shape[1]
     for i in range(b-1):
-        tbl += str(data.columns[i]) + " & " 
-    tbl += str(data.columns[b-1]) + " \\\\ \n"
+        tbl += f"{data.columns[i]:.5f}" + " & " 
+    tbl += f"{data.columns[b-1]:.5f}" + " \\\\ \n"
     for i in range(a):
         for j in range(b-1):
-            tbl += str(arr[i][j]) + " & "
-        tbl += str(arr[i][b-1]) + " \\\\ \n"
+            tbl += f"{arr[i][j]:.5f}" + " & "
+        tbl += f"{arr[i][b-1]:.5f}" + " \\\\ \n"
     return tbl
 
 # primjer
